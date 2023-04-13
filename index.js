ca006c98cb0ff2b34bbf6f18318f89b76edf1cf5
Amanda Portela <mandaptela@gmail.com>
2023-04-12 23:39:47 -0300

    Update/refator basic functions already existents

--- index.js
+++ index.js
@@ -1,46 +1,88 @@
-const cores = document.querySelector('#color-palette');
+const areaCores = document.getElementById('color-palette');
+const coresUm = document.getElementsByTagName('span');
 const pixels = document.querySelector('#pixel-board');
+const noPixelBoard = document.getElementById('pixel-board-area');
 const pixelsUm = document.querySelector('.pixel');
 const apagar = document.getElementById('clear-board');
+const botaoGerarQuadro = document.getElementById('generate-board-size');
+const option = document.querySelectorAll('.option');
+let tamanho = document.getElementById('select').value;
+// const areaCores = document.getElementById("colors-set-area");
 
-function pegarCor(ev) {
-  // resolvida com ajuda do Tássio da turma 19
-  let selecionado = document.querySelector('.selected');
-  selecionado.classList.remove('selected');
-  ev.target.classList.add('selected');
+let column = 5;
+let row = 5;
+
+window.onload = () => {
+  const initialText = 'Escolha um tamanho e comece a PInXELar';
+  const initialElement = document.createElement('h1');
+  pixels.appendChild(initialElement);
+  pixels.className = 'initial-state';
+  initialElement.className = 'initial-text';
+  initialElement.textContent = initialText;
+};
+
+const cores = ['Rosa', 'Verde', 'Azul', 'Roxo', 'Amarelo', 'Laranja', 'Vermelho',
+  'Preto', 'Cinza', 'Branco'];
+cores.forEach((cor) => {
+  const colorElement = document.createElement('span');
+  areaCores.appendChild(colorElement);
+  colorElement.className = 'color';
+  colorElement.id = cor;
+});
+const padrao = document.getElementById('Preto');
+padrao.classList.add('selected');
+
+option.forEach((option) => {
+  option.addEventListener('click', () => {
+    column = option.value.split('x')[0];
+    row = option.value.split('x')[1];
+  });
+});
+
+botaoGerarQuadro.addEventListener('click', () => {
+  const initialEl = document.querySelector('.initial-text');
+  pixels.removeChild(initialEl);
+  apagarTudo();
+  pixels.className = `x${row}`;
+  for (let i = 0; i < Number(column) * Number(row);) {
+    board();
+    i += 1;
+  }
+});
+
+function board() {
+  const board = document.createElement('article');
+  pixels.appendChild(board);
+  board.classList.add('pixel');
+
+  if (pixels.className === `x` || pixels.className === `x5`) board.classList.add(`tam-x5`);
+  if (pixels.className === `x15`) board.classList.add(`tam-x15`);
+  if (pixels.className === `x25`) board.classList.add(`tam-x25`);
 }
-cores.addEventListener('click', pegarCor);
+
+cores.forEach((index) => coresUm[index]
+  .addEventListener('click', (ev) => {
+    let selecionado = document.querySelector('.selected');
+    selecionado.classList.remove('selected');
+    ev.target.classList.add('selected');
+  }));
 
 function pintarPixel(ev) {
-  for (let i = 0; i <= 25; ) {
-    if (cores.children[0].classList.contains('selected')) {
-      ev.target.classList.add('pixelPreto');
-      i += 1;
-    } 
-		else if (cores.children[1].classList.contains('selected')) {
-      ev.target.classList.add('pixelVerde');
-      i += 1;
-    } 
-		else if (cores.children[2].classList.contains('selected')) {
-      ev.target.classList.add('pixelRosa');
-      i += 1;
-    } 
-		else if (cores.children[3].classList.contains('selected')) {
-      ev.target.classList.add('pixelAzul');
-      i += 1;
-    } 
-		else {
-      ev.target.style.backgroudColor = 'red';
-      i += 1;
-    }
+  const coresPaleta = cores.map((index) => coresUm[index]);
+  const selected = coresPaleta.find((i) => i.className.includes('selected'))
+  for (let i = 0; i < coresPaleta.length;) {
+    ev.target.className = `pixel${selected.id}x${row}`;
+    i += 1;
   }
 }
 pixels.addEventListener('click', pintarPixel);
 
-function apagarTudo(ev) {
-  for (i = 0; i < pixels; ) {
-    pixelsUm.classList.add('quadroLimpo');
+function apagarTudo() {
+  for (let i = 0; i < pixels.children.length;) {
+    pixels.children[i].className = 'pixel';
+    pixels.children[i].classList.add(`tam-x${row}`);
     i += 1;
   }
 }
 apagar.addEventListener('click', apagarTudo);
+
